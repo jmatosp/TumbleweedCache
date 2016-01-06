23039f59b96a35027d4cfac7c2cbdc653024f192
Jose Pinto <bluecor@gmail.com>
2016-01-06 11:25:08 +0000

    added more tests

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -25,6 +25,24 @@ class ItemTest extends PHPUnit_Framework_TestCase
     public function testSetString()
     {
         $item = new Item('my_key', 'value');
+        $this->assertSame('value', $item->get());
+    }
 
+    public function testSetInt()
+    {
+        $item = new Item('my_key', 1);
+        $this->assertSame(1, $item->get());
+    }
+
+    public function testSetArray()
+    {
+        $item = new Item('my_key', [1 => 'hello', 'world' => 2]);
+        $this->assertSame([1 => 'hello', 'world' => 2], $item->get());
+    }
+
+    public function testSetObject()
+    {
+        $item = new Item('my_key', new stdClass());
+        $this->assertEquals(new stdClass(), $item->get());
     }
 }
